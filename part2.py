b48a922f628be65f2f2e55deebe54973d4b5c772
jpm0112 <jpm0112@auburn.edu>
2025-03-27 02:07:26 -0500

    part 2 done

--- part2.py
+++ part2.py
@@ -3,45 +3,73 @@ import numpy as np
 data = np.loadtxt('Case2data.csv', delimiter=',', skiprows=1)
 
 
-def calculate_average_cost(data, s):
+def calculate_average_cost(data, s, t, ctt_matrix):
 
     lam = data[s,1]
     k = data[s,2]
     c = data[s,3]
     h = data[s,4]
 
-    t = len(data)
+    if ctt_matrix[s,t] != 0:
+        return ctt_matrix[s, t]
+
     value = 0
     for i in range(t-s):
         for j in range(t-i):
             value = value + data[j,1]
 
     value = value * h
-
     value = value + k
-
     value = value /(t-s+1)
 
 
     # add purchasing cost:
-
     purchasing_cost = 0
     for i in range(t-s):
         purchasing_cost = purchasing_cost + data[i,1]*c
-
     value = value + purchasing_cost
 
-
-    return (value)
-
+    return value
 
 
-s = 1
-t = 2
 
-qs = data[0,1]
-q1 = data[0,1]
-c11 = k
+s = 0
+t = 1
 
-for t in range(1,len(data)):
-    avg_c = calculate_average_cost(data, s)
+q_list = np.zeros(len(data))
+q_list[s] = data[0,1]
+ctt_matrix = np.zeros([len(data), len(data)])
+ctt_matrix[0,0] = data[0,2]
+
+for t in range(len(data)):
+
+    if calculate_average_cost(data, s, t, ctt_matrix) < calculate_average_cost(data, s, t - 1, ctt_matrix):
+        q_list[s] = q_list[s] + data[t, 1]
+    else:
+        s = t
+        q_list[t] = data[t, 1]
+        ctt_matrix[t,t] = data[s,2]
+
+    t = t + 1
+
+print(q_list)
+
+purchase_cost = 0
+for i in range(len(q_list)):
+    purchase_cost =purchase_cost + q_list[s]*data[i,1]
+
+print(purchase_cost)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
